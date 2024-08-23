fe53564f9d7aed6be8afca9a355adadd0984b62b
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-23 21:55:30 +0300

    feat(counter): avoid Zone Pollution in Counter Component

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, NgZone, OnInit, signal } from '@angular/core';
 
 import { InfoMessageComponent } from '../info-message/info-message.component';
 
@@ -9,12 +9,24 @@ import { InfoMessageComponent } from '../info-message/info-message.component';
   styleUrl: './counter.component.css',
   imports: [InfoMessageComponent],
 })
-export class CounterComponent {
+export class CounterComponent implements OnInit {
   count = signal(0);
-
+  private zone = inject(NgZone);
   get debugOutput() {
     console.log('[Counter] "debugOutput" binding re-evaluated.');
     return 'Counter Component Debug Output';
+  }
+
+  ngOnInit() {
+    setInterval(() => {
+      this.count.set(0);
+    }, 4000);
+
+    this.zone.runOutsideAngular(() => {
+      setInterval(() => {
+        console.log('Timer Expired!');
+      }, 5000);
+    });
   }
 
   onDecrement() {
